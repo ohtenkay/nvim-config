ab6ed72484fdd85e321efc59ed67e4a0c733fee9
Ondřej Hložek <hlozek.o1@gmail.com>
2024-11-01 12:20:19 +0100

    removed unused keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,12 +1,5 @@
 local set = vim.keymap.set
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
-set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 -- Clear hlsearch on pressing <Esc> in normal mode
 set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
