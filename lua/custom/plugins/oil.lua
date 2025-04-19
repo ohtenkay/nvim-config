1cff4225ccd0e906159db88495990915f77437b0
Ondřej Hložek <hlozek.o1@gmail.com>
2025-04-19 02:03:12 +0200

    feat: add preview mode to oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -23,7 +23,7 @@ return {
       }
 
       -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+      vim.keymap.set('n', '-', '<CMD>Oil --preview<CR>', { desc = 'Open parent directory' })
     end,
   },
 }
