eec09ff4ea4bfffbff912817c9625e4a46fdce1e
Ondřej Hložek <hlozek.o1@gmail.com>
2025-09-07 00:16:57 +0200

    feat: add more beautiful colors

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,9 @@
 return {
   'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    'xzbdmw/colorful-menu.nvim',
+  },
   version = '1.*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -9,7 +12,27 @@ return {
       preset = 'default',
       ['<Right>'] = { 'select_and_accept' },
     },
-    completion = { documentation = { auto_show = true, auto_show_delay_ms = 200 } },
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 200 },
+      menu = {
+        draw = {
+          gap = 8,
+          -- We don't need label_description now because label and label_description are already
+          -- combined together in label by colorful-menu.nvim.
+          columns = { { 'label' }, { 'kind_icon', 'kind', gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require('colorful-menu').blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require('colorful-menu').blink_components_highlight(ctx)
+              end,
+            },
+          },
+        },
+      },
+    },
     signature = {
       enabled = true,
       trigger = {
