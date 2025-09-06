022137c3e7aaa2188bd1b6fcfb4edc97af88463a
Ondřej Hložek <hlozek.o1@gmail.com>
2025-09-06 18:10:56 +0200

    format: the whole thing

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,8 +1,8 @@
 -- Bootstrap lazy
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
+require('lazy').setup {
   spec = {
     -- import plugins
     { import = 'plugins' },
@@ -24,5 +24,4 @@ require('lazy').setup({
   install = { colorscheme = { 'kanagawa' } },
   -- automatically check for plugin updates
   checker = { enabled = true },
-})
-
+}
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -11,21 +11,21 @@ return {
           ['<C-l>'] = false,
           ['<C-k>'] = false,
           ['<C-j>'] = false,
-          ["gd"] = {
-              desc = "Toggle file detail view",
-              callback = function()
-                  detail = not detail
-                  if detail then
-                      require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
-                  else
-                      require("oil").set_columns({ "icon" })
-                  end
-              end,
+          ['gd'] = {
+            desc = 'Toggle file detail view',
+            callback = function()
+              detail = not detail
+              if detail then
+                require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+              else
+                require('oil').set_columns { 'icon' }
+              end
+            end,
           },
         },
         win_options = {
           signcolumn = 'yes',
-              winbar = "%!v:lua.get_oil_winbar()",
+          winbar = '%!v:lua.get_oil_winbar()',
         },
         skip_confirm_for_simple_edits = true,
         watch_for_changes = true,
@@ -37,19 +37,19 @@ return {
         },
         lsp_file_methods = {
           autosave_changes = 'unmodified',
-        }
+        },
       }
 
       -- Declare a global function to retrieve the current directory
       function _G.get_oil_winbar()
-          local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-          local dir = require("oil").get_current_dir(bufnr)
-          if dir then
-              return vim.fn.fnamemodify(dir, ":~")
-          else
-              -- If there is no current directory (e.g. over ssh), just show the buffer name
-              return vim.api.nvim_buf_get_name(0)
-          end
+        local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+        local dir = require('oil').get_current_dir(bufnr)
+        if dir then
+          return vim.fn.fnamemodify(dir, ':~')
+        else
+          -- If there is no current directory (e.g. over ssh), just show the buffer name
+          return vim.api.nvim_buf_get_name(0)
+        end
       end
 
       -- Open parent directory in current window in preview mode
@@ -57,7 +57,7 @@ return {
     end,
   },
   {
-    "benomahony/oil-git.nvim",
-    dependencies = { "stevearc/oil.nvim" },
-  }
+    'benomahony/oil-git.nvim',
+    dependencies = { 'stevearc/oil.nvim' },
+  },
 }
