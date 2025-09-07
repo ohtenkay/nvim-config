3fec3b1de498651798631ee7fcb9a6160fff02ac
Ondřej Hložek <hlozek.o1@gmail.com>
2025-09-07 23:47:48 +0200

    feat: add autopair support

---
+++ lua/plugins/pairs.lua
@@ -0,0 +1,8 @@
+return {
+  'saghen/blink.pairs',
+  version = '*', -- (recommended) only required with prebuilt binaries
+  dependencies = 'saghen/blink.download', -- prebuilt binaries
+  --- @module 'blink.pairs'
+  --- @type blink.pairs.Config
+  opts = {},
+}
