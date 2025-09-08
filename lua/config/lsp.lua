3b93f2446fc39ab79d72f2432adb3d4d882593d3
Ondřej Hložek <hlozek.o1@gmail.com>
2025-09-09 00:51:40 +0200

    feat: enable words and remove custom logic for less functionality

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,22 +1,3 @@
 vim.lsp.enable {
   'lua_ls',
 }
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('lsp-attach-custom', { clear = true }),
-  callback = function(event)
-    -- Highlight references when resting cursor
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        buffer = event.buf,
-        callback = vim.lsp.buf.document_highlight,
-      })
-
-      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-        buffer = event.buf,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
-  end,
-})
