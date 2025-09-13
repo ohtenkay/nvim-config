f547bcacdb02fc43e0f1151c5fa8e8473b1a9a9e
Ondřej Hložek <hlozek.o1@gmail.com>
2025-09-14 00:04:03 +0200

    feat: add Kotlin lsp support

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,18 @@
 vim.lsp.enable {
   'lua_ls',
+  'kotlin_lsp',
 }
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach-custom', { clear = true }),
+  callback = function(event)
+    -- Clear references when moving cursor
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client.server_capabilities.documentHighlightProvider then
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
+  end,
+})
