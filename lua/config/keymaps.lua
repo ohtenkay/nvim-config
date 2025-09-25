44ab50b3a9768dae2f512861d4b4b813eb4a4cf8
Ondřej Hložek <hlozek.o1@gmail.com>
2025-09-25 20:28:18 +0200

    feat: diagnostic and go to definition keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,9 @@ set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Center the cursor when scrolling files
 set('n', '<C-d>', '<C-d>zz')
 set('n', '<C-u>', '<C-u>zz')
+
+-- Show diagnostic messages in a floating window
+set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+
+-- Go to definition
+set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to [D]efinition' })
