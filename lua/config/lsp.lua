a4226ef00702b803573888395787f710863f593c
Ondřej Hložek <hlozek.o1@gmail.com>
2025-11-30 12:10:28 +0100

    feat: enable language servers and disable clear on move

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,18 +2,17 @@ vim.lsp.enable {
   'lua_ls',
   'kotlin_lsp',
   'ts_ls',
+  'svelte',
+  'tailwindcss',
 }
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('lsp-attach-custom', { clear = true }),
-  callback = function(event)
-    -- Clear references when moving cursor
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-        buffer = event.buf,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
-  end,
-})
+--
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   group = vim.api.nvim_create_augroup('lsp-attach-custom', { clear = true }),
+--   callback = function(event)
+--     -- Clear references when moving cursor
+--     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+--       buffer = event.buf,
+--       callback = Snacks.words.clear(),
+--     })
+--   end,
+-- })
