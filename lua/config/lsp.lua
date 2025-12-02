ee762c0dcdedf8f7ea414217ae40d424d0df3a90
Ondřej Hložek <hlozek.o1@gmail.com>
2025-12-02 15:02:54 +0100

    feat: delete commented out code

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,14 +5,3 @@ vim.lsp.enable {
   'svelte',
   'tailwindcss',
 }
---
--- vim.api.nvim_create_autocmd('LspAttach', {
---   group = vim.api.nvim_create_augroup('lsp-attach-custom', { clear = true }),
---   callback = function(event)
---     -- Clear references when moving cursor
---     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
---       buffer = event.buf,
---       callback = Snacks.words.clear(),
---     })
---   end,
--- })
